8ece28216889cddae958de93a09b5d94925bbeb4
Theba Gomez <gomez.teba@gmail.com>
2020-11-11 17:05:13 +0100

    feat(): Created Navbar component with styles and added to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import Navbar from './components/Navbar'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>ISS app</h1>
+      <Navbar />
       </header>
     </div>
   );
