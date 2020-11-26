750048c34967d8c9f33ab2161d16c8cf54fe9034
Theba Gomez <gomez.teba@gmail.com>
2020-11-26 14:39:30 +0100

    feat(): Added Pass Times fetch funcion and a proxy

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,6 +1,8 @@
-const getData = (url) => fetch(url).then(res => res.json())
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const getData = (url) => fetch(proxy + url).then(res => res.json())
 
 const getAstros = () => getData('http://api.open-notify.org/astros.json')
 const getLocation = () => getData('http://api.open-notify.org/iss-now.json')
+const getPassTimes = (lat, lon) => getData(`http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${lon}`)
 
-module.exports = { getAstros, getLocation }
+module.exports = { getAstros, getLocation, getPassTimes }
